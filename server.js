7e655c85fcc27e51bbf894ce60ca7a0737b48301
k-gard <std080064@ac.eap.gr>
2021-06-07 19:53:48 +0300

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/employee-app'));
+app.use(express.static('./dist/employee-app'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/employee-app/index.html'));
+    res.sendFile(path.join('./dist/employee-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
